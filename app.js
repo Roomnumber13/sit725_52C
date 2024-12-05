befcfd5e6cdc932c005c8e36172a0f2d01d72211
Roomnumber13 <rajkumar.rajendran197@gmail.com>
2024-12-05 23:11:10 +1100

    Update file

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 app.use('/api', documentRoutes);
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index2.html');
 });
 
 initializeDatabase().then(() => {
