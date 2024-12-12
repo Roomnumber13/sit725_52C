62ebfd2873c38ac4a63a5a1fec814aea69e71105
Roomnumber13 <rajkumar.rajendran197@gmail.com>
2024-12-12 11:44:12 +1100

    Update file

--- app.js
+++ app.js
@@ -26,3 +26,12 @@ initializeDatabase().then(() => {
 }).catch(err => {
     console.error('Failed to initialize database:', err);
 });
+app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
+    var firstNumber = parseInt(req.params.firstNumber) 
+    var secondNumber = parseInt(req.params.secondNumber)
+    var result = firstNumber + secondNumber || null
+    if(result == null) {
+      res.json({result: result, statusCode: 400}).status(400)
+    }
+    else { res.json({result: result, statusCode: 200}).status(200) } 
+  })
